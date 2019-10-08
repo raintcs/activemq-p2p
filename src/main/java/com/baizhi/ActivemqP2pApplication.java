523da6de12bff26cafc069284ed5892f643c0903
raintcs <tcszzu@126.com>
2019-10-08 21:58:56 +0800

    v3.0

--- src/main/java/com/baizhi/ActivemqP2pApplication.java
+++ src/main/java/com/baizhi/ActivemqP2pApplication.java
@@ -16,6 +16,7 @@ public class ActivemqP2pApplication {
         System.out.println("he is a good man!");
          System.out.println("good");
         System.out.println("我是谈昌森，今年18");
+        System.out.println("天下第一我怕谁！");
     }
 
 

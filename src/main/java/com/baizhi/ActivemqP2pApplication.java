0bd0ab2bff3aca8c3e753a5f561493958fca1b94
raintcs <tcszzu@126.com>
2019-10-08 20:17:00 +0800

    init v1.0

--- src/main/java/com/baizhi/ActivemqP2pApplication.java
+++ src/main/java/com/baizhi/ActivemqP2pApplication.java
@@ -9,6 +9,8 @@ public class ActivemqP2pApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActivemqP2pApplication.class, args);
+        System.out.println("ttt");
     }
+
 
 }
